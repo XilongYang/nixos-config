672f5565327118f74ea0cc3e6b0f8905029cadc5
Xilong Yang <xilong.yang@foxmail.com>
2023-09-03 22:25:27 +0800

    dark mode & font scale

--- home.nix
+++ home.nix
@@ -21,7 +21,7 @@
   dconf.settings = {
     "org/gnome/desktop/interface" = { 
       color-scheme = "prefer-dark";
-      text-scaling-factor = 1.25;
+      text-scaling-factor = 1.5;
     };
   };
 
