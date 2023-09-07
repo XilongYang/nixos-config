a91ee7ed41552d2572231a4065779d90fbb2ba42
Xilong Yang <xilong.yang@foxmail.com>
2023-09-07 23:48:58 +0800

    shortcut && flameshot

--- home.nix
+++ home.nix
@@ -20,6 +20,7 @@ in
     firefox-esr
     obsidian
     telegram-desktop
+    flameshot
   ];
 
   # Gnome Settings
@@ -42,6 +43,27 @@ in
       switch-to-application-7 = [];
       switch-to-application-8 = [];
       switch-to-application-9 = [];
+      show-screen-recording-ui = [];
+      screenshot = [];
+      show-screenshot-ui = [];
+      screenshot-window = [];
+    };
+    "org/gnome/desktop/wm/keybindings" = {
+      show-desktop = ["<Super>d"]; 
+      move-to-workspace-1 = ["<Shift><Super>1"];
+      move-to-workspace-2 = ["<Shift><Super>2"];
+      move-to-workspace-3 = ["<Shift><Super>3"];
+      move-to-workspace-4 = ["<Shift><Super>4"];
+      switch-to-workspace-1 = ["<Super>1"];
+      switch-to-workspace-2 = ["<Super>2"];
+      switch-to-workspace-3 = ["<Super>3"];
+      switch-to-workspace-4 = ["<Super>4"];
+      switch-applications = [];
+      switch-applications-backward = [];
+      switch-windows = ["<Alt>Tab"];
+      switch-windows-backward = ["<Shift><Alt>Tab"];
+      switch-to-workspace-left = ["<Shift><Super>h"];
+      switch-to-workspace-right = ["<Shift><Super>l"];
     };
     "org/gnome/settings-daemon/plugins/media-keys" = { 
       home = [ "<Super>e" ];
