c34338d577c4e65aa64046e0b25e7193a2bf5131
Xilong Yang <xilong.yang@foxmail.com>
2023-09-07 21:53:39 +0800

    shortcuts

--- home.nix
+++ home.nix
@@ -37,7 +37,7 @@ in
       search = [ "<Super>r" ];
 
       custom-keybindings = [
-        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0"
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
       ];
     };
     "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
