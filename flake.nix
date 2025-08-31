7589bbe0ca6fa961367e93bee53cda380eb90390
Xilong Yang <xilong.yang@foxmail.com>
2025-08-31 23:53:26 +0900

    update

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "Xilong's NixOS";
+
+  nixConfig = {
+    substituters = [
+      "https://cache.nixos.org"
+    ];
+  };
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+  
+  
+  outputs = inputs@{nixpkgs, home-manager, ...} : {
+    nixosConfigurations = {
+      "nixos" = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hardware.nix
+          ./network.nix
+          ./os.nix
+          ./boot.nix
+
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.xilong = import ./home.nix;
+          }
+        ]; 
+      };
+    }; 
+  };
+}
