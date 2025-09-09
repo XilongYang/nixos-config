04a10fb8acb85d4ae7b8eb394e83188533609b57
Xilong Yang <xilong.yang@foxmail.com>
2025-09-10 01:46:30 +0900

    Switch to Hyprland

--- home.nix
+++ home.nix
@@ -5,7 +5,7 @@ in
 {
   home.username = "xilong";
   home.homeDirectory = "/home/xilong";
-
+  
   programs.git = {
     enable = true;
     userName = "Xilong Yang";
@@ -38,15 +38,25 @@ in
     chez
     eudic
     fastfetch
-    firefox-esr
+    fuzzel
     gh
-    gnome-tweaks
     google-chrome
+    hyprpaper
     foliate
+    kitty
+    libsForQt5.qtstyleplugin-kvantum
     mpv
     neovim
+    qogir-theme
+    qogir-icon-theme
+    qogir-kde
+    libsForQt5.qt5ct
+    qt6ct
     rclone
     sioyek
+    xfce.thunar
+    waybar
+
     #LSPs
     clang-tools
     jdt-language-server
@@ -60,87 +70,246 @@ in
     marksman
   ];
 
-  # Gnome Settings
-  dconf.settings = {
-    "org/gnome/desktop/interface" = { 
-      color-scheme = "prefer-dark";
-    };
-    "org/gnome/shell" = {
-      favorite-apps = [
-        "google-chrome.desktop"
-        "sioyek.desktop"
-        "com.github.johnfactotum.Foliate.desktop"
-        "eusoft-eudic.desktop"
-        "org.gnome.Console.desktop"
-        "org.gnome.Nautilus.desktop"
-      ];
-    };
-    "org/gnome/desktop/input-sources" = { 
-      sources = [ (mkTuple ["xkb" "us"]) (mkTuple ["ibus" "rime"]) ];
-      per-window = false;
-    };
-    "org/gnome/shell" = {
-      enabled-extensions = [];
-    };
-    "org/gnome/shell/keybindings" = {
-      switch-to-application-1 = [];
-      switch-to-application-2 = [];
-      switch-to-application-3 = [];
-      switch-to-application-4 = [];
-      switch-to-application-5 = [];
-      switch-to-application-6 = [];
-      switch-to-application-7 = [];
-      switch-to-application-8 = [];
-      switch-to-application-9 = [];
-      show-screen-recording-ui = [];
-      screenshot = ["Print"];
-      show-screenshot-ui = ["<Shift><Super>s"];
-      screenshot-window = [];
-    };
-    "org/gnome/desktop/wm/keybindings" = {
-      show-desktop = ["<Super>d"]; 
-      move-to-workspace-1 = ["<Shift><Super>1"];
-      move-to-workspace-2 = ["<Shift><Super>2"];
-      move-to-workspace-3 = ["<Shift><Super>3"];
-      move-to-workspace-4 = ["<Shift><Super>4"];
-      switch-to-workspace-1 = ["<Super>1"];
-      switch-to-workspace-2 = ["<Super>2"];
-      switch-to-workspace-3 = ["<Super>3"];
-      switch-to-workspace-4 = ["<Super>4"];
-      switch-applications = [];
-      switch-applications-backward = [];
-      switch-windows = ["<Alt>Tab"];
-      switch-windows-backward = ["<Shift><Alt>Tab"];
-      switch-to-workspace-left = ["<Shift><Super>h"];
-      switch-to-workspace-right = ["<Shift><Super>l"];
-      minimize = ["<Shift><Super>j"];
-      maximize = ["<Super>k"];
-      unmaximize = ["<Super>j"];
-      toggle-fullscreen = ["<Super>f"];
-      switch-input-source = ["<Super>space"];
-      switch-input-source-backward = ["<Shift><Super>space"];
-    };
-    "org/gnome/desktop/input-sources" = { 
-      xkb-options = ["lv3:ralt_alt"];
-    };
-    "org/gnome/mutter/keybindings" = { 
-      toggle-tiled-left = ["<Super>h"];
-      toggle-tiled-right = ["<Super>l"];
-    };
-    "org/gnome/settings-daemon/plugins/media-keys" = { 
-      home = [ "<Super>e" ];
-      search = [ "<Super>r" ];
-      control-center = [ "<Super>i" ];
-      screensaver = [ "<Control><Alt>l" ];
+  xdg.configFile."waybar/config".text = ''
+    {
+        "layer": "top",
+        "position": "top",
+        "height": 24,
+        "spacing": 5,
+    
+        "modules-left": ["hyprland/workspaces"],
+        "modules-center": [],
+        "modules-right": ["backlight", "wireplumber", "battery", "hyprland/language", "idle_inhibitor", "network", "bluetooth", "tray", "clock"],
+    
+        "hyprland/workspaces": {
+            "format": "<span size='larger'>{icon}</span>",
+            "on-click": "activate",
+            "format-icons": {
+                "active": "\uf444",
+                "default": "\uf4c3"
+            },
+            "icon-size": 10,
+            "sort-by-number": true,
+            "persistent-workspaces": {
+                "1": [],
+                "2": [],
+                "3": [],
+                "4": [],
+                "5": [],
+            }
+        },
+    
+        "clock": {
+            "format": "{:%H:%M}"
+        },
+    
+        "backlight": {
+            "format": " {icon}  {percent}% ",
+            "format-icons": ["","󰃜", "󰃛", "󰃞","󰃝","󰃟","󰃠"],
+            "tooltip": false
+        },
+    
+        "wireplumber": {
+            "format": " \udb81\udd7e  {volume}% ",
+            "max-volume": 100,
+            "scroll-step": 5
+        },
+    
+        "battery": {
+            "bat": "BAT1",
+            "interval": 60,
+            "format": "{icon}  {capacity}%",
+            "format-icons": ["\uf244", "\uf243", "\uf242", "\uf241", "\uf240"],
+        },
+    
+        "memory": {
+            "interval": 30,
+            "format": "\uf4bc  {used:0.1f}G"
+        },
+    
+        "temperature": {
+            "format": "{temperatureC}°C"
+        },
+    
+        "network": {
+            "format": "",
+            "format-ethernet": "\udb83\udc9d",
+            "format-wifi": "{icon}",
+            "format-disconnected": "\udb83\udc9c",
+            "format-icons": ["\udb82\udd2f", "\udb82\udd1f", "\udb82\udd22", "\udb82\udd25", "\udb82\udd28"],
+            "tooltip-format-wifi": "{essid} ({signalStrength}%)",
+            "tooltip-format-ethernet": "{ifname}",
+            "tooltip-format-disconnected": "Disconnected",
+        },
+    
+        "bluetooth": {
+            "format": "\udb80\udcaf",
+            "format-disabled": "\udb80\udcb2",
+            "format-connected": "\udb80\udcb1",
+            "tooltip-format": "{controller_alias}\t{controller_address}",
+            "tooltip-format-connected": "{controller_alias}\t{controller_address}\n\n{device_enumerate}",
+            "tooltip-format-enumerate-connected": "{device_alias}\t{device_address}"
+        },
+    
+        "hyprland/language": {
+            "format": "{short}"
+        },
+    
+        "tray": {
+            "icon-size": 16,
+            "spacing": 16
+        },
+    
+        "custom/platform-profile": {
+    	"format": "{icon} ",
+    	"exec": "~/.config/waybar/platform_profile.sh",
+    	"return-type": "json",
+    	"restart-interval": 1,
+    	"format-icons": {
+    	    "quiet": "\udb80\udf2a",
+    	    "balanced": "\udb80\ude10",
+    	    "performance": "\uf427",
+                "default": "?"
+    	},
+        },
+    
+        "idle_inhibitor": {
+            "format": "{icon}",
+            "format-icons": {
+                "activated": "\udb80\udd76",
+                "deactivated": "\udb83\udfaa"
+            }
+        }
+    }
+  '';
 
-      custom-keybindings = [
-        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
-      ];
+  xdg.configFile."waybar/style.css".text = ''
+    @define-color foreground #eeeeee;
+    @define-color foreground-inactive #aaaaaa;
+    @define-color background #000000;
+    
+    * {
+        font-family: JetBrainsMono Nerd Font;
+        font-size: 17px;
+        padding: 0;
+        margin: 0;
+    }
+    
+    #waybar {
+        color: @foreground;
+        background-color: @background;
+    }
+    
+    #workspaces button {
+        color: @foreground;
+        padding-left: 0.7em;
+    }
+    
+    #workspaces button.empty {
+        color: @foreground-inactive;
+    }
+    
+    #memory,
+    #custom-platform-profile {
+        padding-left: 1em
+    }
+    
+    #wireplumber,
+    #battery,
+    #idle_inhibitor,
+    #language,
+    #network,
+    #bluetooth,
+    #tray {
+        padding-right: 1em 
+    }
+  '';
+
+  xdg.configFile."hypr/hyprpaper.conf".text = ''
+    preload   = /home/xilong/Pictures/Wallpaper/railway.jpg
+    wallpaper = ,/home/xilong/Pictures/Wallpaper/railway.jpg
+  '';
+
+  xdg.configFile."hypr/hyprland.conf".text = ''
+    $mod = "SUPER"
+    exec-once = "hyprpaper"
+    exec-once = "waybar"
+
+    bind = $mod, RETURN, exec, kitty
+    bind = $mod, R, exec, fuzzel
+    bind = $mod, E, exec, thunar
+    bind = ALT, S, resizeactive,
+    bind = ALT, F4, killactive,
+
+    # Move focus with mod + hjkl
+    bind = $mod, H, movefocus, l
+    bind = $mod, L, movefocus, r
+    bind = $mod, J, movefocus, d
+    bind = $mod, K, movefocus, u
+
+    # Switch workspaces with mod + [0-9]
+    bind = $mod, 1, workspace, 1
+    bind = $mod, 2, workspace, 2
+    bind = $mod, 3, workspace, 3
+    bind = $mod, 4, workspace, 4
+    bind = $mod, 5, workspace, 5
+    bind = $mod, 6, workspace, 6
+    bind = $mod, 7, workspace, 7
+    bind = $mod, 8, workspace, 8
+    bind = $mod, 9, workspace, 9
+    bind = $mod, 0, workspace, 10
+    
+    # Move active window to a workspace with mod + SHIFT + [0-9]
+    bind = $mod SHIFT, 1, movetoworkspace, 1
+    bind = $mod SHIFT, 2, movetoworkspace, 2
+    bind = $mod SHIFT, 3, movetoworkspace, 3
+    bind = $mod SHIFT, 4, movetoworkspace, 4
+    bind = $mod SHIFT, 5, movetoworkspace, 5
+    bind = $mod SHIFT, 6, movetoworkspace, 6
+    bind = $mod SHIFT, 7, movetoworkspace, 7
+    bind = $mod SHIFT, 8, movetoworkspace, 8
+    bind = $mod SHIFT, 9, movetoworkspace, 9
+    bind = $mod SHIFT, 0, movetoworkspace, 10
+
+    # Laptop multimedia keys for volume and LCD brightness
+    bindel = ,XF86AudioRaiseVolume, exec, wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+
+    bindel = ,XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-
+    bindel = ,XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
+    bindel = ,XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle
+    bindel = ,XF86MonBrightnessUp, exec, brightnessctl -e4 -n2 set 5%+
+    bindel = ,XF86MonBrightnessDown, exec, brightnessctl -e4 -n2 set 5%-
+
+    input:touchpad:natural_scroll = true
+  '';
+
+  gtk = {
+    enable = true;
+    theme = {
+      name = "Qogir";
+      package = pkgs.qogir-theme;
     };
-    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
-      binding = "<Super>Return";
-      command = "kgx";
-      name = "terminal";
+    iconTheme = {
+      name = "Qogir";
+      package = pkgs.qogir-icon-theme;
+    };
+  };
+
+  qt = {
+    enable = true;
+    platformTheme.name = "qtct";   # use qt5ct/qt6ct control panels
+    style.name    = "kvantum";# tell Qt to use Kvantum
+  };
+
+  programs.kitty = {
+    enable = true;
+    font = {
+      name = "JetBrains Mono";
+      size = 12;
+    };
+    settings = {
+      # extra kitty.conf options
+      cursor_blink_interval = 0;
+      enable_audio_bell = "no";
     };
   };
 
