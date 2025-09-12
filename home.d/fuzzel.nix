5c6af8d6fa811bcf8ebf78393a47ee274d1ef0dd
Xilong Yang <xilong.yang@foxmail.com>
2025-09-13 00:57:39 +0900

    Color: fuzzel; Font size: kitty

--- home.d/fuzzel.nix
+++ home.d/fuzzel.nix
@@ -6,12 +6,12 @@
     dpi-aware=yes
 
     [colors]
-    background=161616ff
-    text=ffffffff
-    match=ee5396ff
-    selection-match=ee5396ff
-    selection=262626ff
-    selection-text=33b1ffff
-    border=525252ff
+    background=1a1b26ee
+    text=a9b1d6ff
+    match=f7768eff
+    selection-match=f7768eff
+    selection=1a1b26ff
+    selection-text=7aa2f7ff
+    border=a9b1d6ff
   '';
 }
