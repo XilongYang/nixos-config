857cb02867e5216508daaedcdd9cacd08fbb76cd
Xilong Yang <contact@xilong.site>
2025-09-13 18:20:07 +0900

    Waybar

--- home.d/waybar.nix
+++ home.d/waybar.nix
@@ -5,11 +5,13 @@
         "layer": "top",
         "position": "top",
         "height": 24,
-        "spacing": 5,
+        "margin-right": 20,
+        "margin-left": 10,
+        "spacing": 15,
     
         "modules-left": ["hyprland/workspaces"],
         "modules-center": ["hyprland/window"],
-        "modules-right": ["backlight", "wireplumber", "idle_inhibitor", "network", "bluetooth", "tray", "battery", "clock"],
+        "modules-right": ["backlight", "wireplumber", "network", "bluetooth", "tray", "idle_inhibitor", "battery", "clock"],
     
         "hyprland/workspaces": {
             "format": "<span size='larger'>{icon}</span>",
@@ -31,50 +33,63 @@
         "hyprland/window": {  
           "format": "{}"
         },
-        "clock": {
-            "format": "{:%H:%M} "
-        },
-    
+         "clock": {
+           "format": "\uf017 {:%H:%M}",
+           "format-alt": "\ueab0 {:%Y-%m-%d %R}",
+           "tooltip-format": "<tt><small>{calendar}</small></tt>",
+           "calendar": {
+             "mode"          : "month",
+             "mode-mon-col"  : 3,
+             "weeks-pos"     : "right",
+             "on-scroll"     : 1,
+             "format": {
+               "months":     "<span color='#bb9af7'><b>{}</b></span>",
+               "days":       "<span color='#a9b1d6'><b>{}</b></span>",
+               "weeks":      "<span color='#3d59a1'><b>W{}</b></span>",
+               "weekdays":   "<span color='#7aa2f7'><b>{}</b></span>",
+               "today":      "<span color='#f7768e'><b><u>{}</u></b></span>"
+               }
+             },
+           "actions":  {
+             "on-click-right": "mode",
+             "on-scroll-up": "tz_up",
+             "on-scroll-down": "tz_down",
+             "on-scroll-up": "shift_up",
+             "on-scroll-down": "shift_down"
+             }
+         },
         "backlight": {
-            "format": " {icon}  {percent}% ",
+            "format": "{icon} {percent}%",
             "format-icons": ["󰃠"],
             "tooltip": false
         },
-    
         "wireplumber": {
-            "format": " \udb81\udd7e  {volume}% ",
+            "format": "\udb81\udd7e {volume}%",
             "format-muted": "\udb81\udf5f",
             "max-volume": 100,
             "scroll-step": 5
         },
-    
         "battery": {
             "bat": "BAT0",
             "interval": 60,
+            "states": {
+               "warning": 10,
+               "critical": 5
+            },
             "format": "{icon}  {capacity}%",
+            "format-alt": "{icon}  {time}",
             "format-icons": ["\uf244", "\uf243", "\uf242", "\uf241", "\uf240"],
         },
-    
-        "memory": {
-            "interval": 30,
-            "format": "\uf4bc  {used:0.1f}G"
-        },
-    
-        "temperature": {
-            "format": "{temperatureC}°C"
-        },
-    
         "network": {
             "format": "",
-            "format-ethernet": "\udb83\udc9d",
+            "format-ethernet": "\udb83\udc9d ",
             "format-wifi": "{icon}",
-            "format-disconnected": "\udb83\udc9c",
-            "format-icons": ["\udb82\udd2f", "\udb82\udd1f", "\udb82\udd22", "\udb82\udd25", "\udb82\udd28"],
+            "format-disconnected": "\udb83\udc9c ",
+            "format-icons": ["\udb82\udd2f ", "\udb82\udd1f ", "\udb82\udd22 ", "\udb82\udd25 ", "\udb82\udd28 "],
             "tooltip-format-wifi": "{essid} ({signalStrength}%)",
             "tooltip-format-ethernet": "{ifname}",
             "tooltip-format-disconnected": "Disconnected",
         },
-    
         "bluetooth": {
             "format": "\udb80\udcaf",
             "format-disabled": "\udb80\udcb2",
@@ -83,17 +98,15 @@
             "tooltip-format-connected": "{controller_alias}\t{controller_address}\n\n{device_enumerate}",
             "tooltip-format-enumerate-connected": "{device_alias}\t{device_address}"
         },
-    
         "tray": {
             "icon-size": 16,
             "spacing": 16
         },
-    
         "idle_inhibitor": {
             "format": "{icon}",
             "format-icons": {
-                "activated": "\udb80\udd76",
-                "deactivated": "\udb83\udfaa"
+                "activated": "\udb80\udd76 ",
+                "deactivated": "\udb83\udfaa "
             }
         }
     }
@@ -118,26 +131,11 @@
     
     #workspaces button {
         color: @foreground;
-        padding-left: 0.7em;
+        padding-right: 10px;
     }
     
     #workspaces button.empty {
         color: @foreground-inactive;
-    }
-    
-    #memory,
-    #custom-platform-profile {
-        padding-left: 1em
-    }
-    
-    #wireplumber,
-    #battery,
-    #idle_inhibitor,
-    #language,
-    #network,
-    #bluetooth,
-    #tray {
-        padding-right: 1em 
     }
   '';
 }
