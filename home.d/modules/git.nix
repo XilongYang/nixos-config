754357f317c9258dbe1486f0b9d196dc0dae56c4
Xilong Yang <contact@xilong.site>
2026-01-05 17:32:00 +0900

    Git: GPG Signing

--- home.d/modules/git.nix
+++ home.d/modules/git.nix
@@ -5,6 +5,11 @@
     settings = {
       user.name = "Xilong Yang";
       user.email = "contact@xilong.site";
+      user.signingkey = "1D3659FB3DEC56A5844668EA1E1BBEE5D3C8516C";
+
+      commit.gpgsign = true;
+      gpg.program = "gpg";
+
       init.defaultBranch = "main";
     };
   };
