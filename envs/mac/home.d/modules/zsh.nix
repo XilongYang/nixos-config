247d2735a50935806f16519d6f22b3ef2ac4d39f
Xilong Yang <contact@xilong.site>
2026-01-23 15:05:06 +0800

    Mac: Add nix daemon to zsh

--- envs/mac/home.d/modules/zsh.nix
+++ envs/mac/home.d/modules/zsh.nix
@@ -13,6 +13,9 @@
     # macOS 需要这个防止 gpg 弹窗抽风
     initContent = ''
       export GPG_TTY=$(tty)
+      if [ -e /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh ]; then
+        . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+      fi
     '';
 
     oh-my-zsh = {
