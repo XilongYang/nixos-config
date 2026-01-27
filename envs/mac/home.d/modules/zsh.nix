efcf31f67b147d53b92faec61097cb5a99bc4d52
Xilong Yang <contact@xilong.site>
2026-01-27 13:10:47 +0800

    Zsh: color

--- envs/mac/home.d/modules/zsh.nix
+++ envs/mac/home.d/modules/zsh.nix
@@ -8,6 +8,7 @@
 
     shellAliases = {
       vim = "nvim";
+      ls  = "ls --color=auto";
     };
 
     # macOS 需要这个防止 gpg 弹窗抽风
@@ -16,6 +17,8 @@
       if [ -e /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh ]; then
         . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
       fi
+      export CLICOLOR=1
+      export LSCOLORS=...
     '';
 
     oh-my-zsh = {
