217e4002c895be6b8abe7254e681565738438fc9
Xilong Yang <contact@xilong.site>
2026-03-07 21:50:35 +0900

    nvim:clangd:query-driver

--- assets/nvim/lsp/clangd.lua
+++ assets/nvim/lsp/clangd.lua
@@ -8,6 +8,7 @@ return {
     "--header-insertion=iwyu",
     "--pch-storage=memory",
     "--function-arg-placeholders=true",
+    "--query-driver=/nix/store/*/bin/clang,/nix/store/*/bin/gcc",
   },
   root_markers = { "compile_commands.json", "compile_flags.txt", ".git", "CMakeLists.txt" },
   filetypes = { "c", "cpp", "objc", "objcpp", "cuda" },
