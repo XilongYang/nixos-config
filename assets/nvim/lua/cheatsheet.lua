7c21f7438c54a45970abf8da7ad770ae976a4cb4
Xilong Yang <contact@xilong.site>
2026-03-20 19:39:46 +0900

    Nvim: Cheatsheet: Display in center

--- assets/nvim/lua/cheatsheet.lua
+++ assets/nvim/lua/cheatsheet.lua
@@ -210,13 +210,20 @@ function M.open()
   local buf = vim.api.nvim_create_buf(false, true)
   vim.api.nvim_buf_set_lines(buf, 0, -1, false, lines)
 
+  local width = math.floor(vim.o.columns * 0.7)
+  local height = math.floor((vim.o.lines - vim.o.cmdheight) * 0.8)
+
+  local row = math.floor((vim.o.lines - height) / 2 - 1)
+  local col = math.floor((vim.o.columns - width) / 2)
+
   local win = vim.api.nvim_open_win(buf, true, {
     relative = "editor",
-    width = math.floor(vim.o.columns * 0.75),
-    height = math.floor(vim.o.lines * 0.85),
-    row = 2,
-    col = 4,
+    width = width,
+    height = height,
+    row = row,
+    col = col,
     border = "rounded",
+    style = "minimal",
   })
 
   render(buf, entries)
