c897fbbfdec78cfed51cb72736b6ed3899570538
Xilong Yang <contact@xilong.site>
2026-03-20 19:29:02 +0900

    Nvim: nvim-tree: add mapping h/l/v

--- assets/nvim/lua/plugins/common/nvim-tree.lua
+++ assets/nvim/lua/plugins/common/nvim-tree.lua
@@ -1,7 +1,23 @@
 return {
   {"nvim-tree/nvim-tree.lua",
     config = function()
-      require("nvim-tree").setup()
+      require("nvim-tree").setup({
+        on_attach = function(bufnr)
+          local api = require("nvim-tree.api")
+
+          local function opts(desc)
+            return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+          end
+
+          -- 保留默认映射
+          api.config.mappings.default_on_attach(bufnr)
+
+          -- 自定义映射
+          vim.keymap.set("n", "l", api.tree.change_root_to_node, opts("CD into directory"))
+          vim.keymap.set("n", "h", api.tree.change_root_to_parent, opts("CD to parent"))
+          vim.keymap.set("n", "v", api.node.open.vertical, opts("Open vertical split"))
+        end,
+      })
     end,
   },
 }
