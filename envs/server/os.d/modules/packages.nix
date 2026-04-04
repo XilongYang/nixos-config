d0da525cfa59b909bfd13a704153c956dd0383ab
Xilong Yang <contact@xilong.site>
2026-04-04 23:55:37 +0900

    Server: remove unused packages

--- envs/server/os.d/modules/packages.nix
+++ envs/server/os.d/modules/packages.nix
@@ -5,64 +5,34 @@
   nixpkgs.config.allowUnfree = true;
   environment.systemPackages = with pkgs; [
     # Base
-    appimage-run
-    bochs
-    clang-tools
-    cloc
-    cmake
-    dev86
-    findutils
-    gcc
-    gdb
-    ghc
     git
     git-filter-repo
-    gnumake
     gnupg
     jq
-    linux-firmware
+    kitty
     mandoc
     nodejs
-    ntfs3g
     openssh
     openssl
     p7zip
     pass
     pinentry-curses
-    powertop
     python3
-    pipx
-    rclone
     ripgrep
-    sof-firmware
     tree
     tmux
-    unrar
     unzip
-    vim
     wget
     zsh
 
     # User
-    aria2
-    chez
     cloudflared
     fastfetch
-    gh
-    kitty
     neovim
     codex
     codex-acp
 
     #LSPs
-    jdt-language-server
-    pyright
     nodePackages.bash-language-server
-    sqls
-    nodePackages.typescript-language-server
-    nodePackages.vscode-langservers-extracted
-    haskell-language-server
-    lua-language-server
-    marksman
   ];
 }
