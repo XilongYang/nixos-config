5151a629dc5e0ce1566638f15aebedd7037ce280
Xilong Yang <contact@xilong.site>
2026-04-05 14:55:18 +0900

    Direnv

--- flake.nix
+++ flake.nix
@@ -2,11 +2,22 @@
   description = "Root router flake (delegates to env flakes)";
 
   inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     server.url  = "path:./envs/server";
     mac.url  = "path:./envs/mac";
   };
 
-  outputs = { self, server, mac, ... }: {
+  outputs = { self, nixpkgs, server, mac, ... }: 
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [
+        lua-language-server
+      ];
+    };
+
     nixosConfigurations =
       (server.nixosConfigurations or {});
 
