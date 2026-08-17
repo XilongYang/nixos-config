546054159417c4fca38576301c874402d2d35c64
Xilong Yang <contact@xilong.site>
2026-08-17 21:00:06 +0900

    nvim: clangd: fix path for g++

--- files/shared/nvim/lsp/clangd.lua
+++ files/shared/nvim/lsp/clangd.lua
@@ -12,7 +12,7 @@ return {
     "--header-insertion=iwyu",
     "--pch-storage=memory",
     "--function-arg-placeholders=true",
-    "--query-driver=/nix/store/*/bin/clang,/nix/store/*/bin/gcc",
+    "--query-driver=/nix/store/*/bin/clang,/nix/store/*/bin/gcc,/nix/store/*/bin/g++,/run/current-system/sw/bin/g++",
   },
   root_markers = { "compile_commands.json", "compile_flags.txt", ".git", "CMakeLists.txt" },
   filetypes = { "c", "cpp", "objc", "objcpp", "cuda" },
